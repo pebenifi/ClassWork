7c23038a7ac7de2d7773554484d1eb85b4892731
Тимофей Напольников <pebenifi@gmail.com>
2021-03-12 17:16:03 +0300

    Реклама с картинкой

--- main.py
+++ main.py
@@ -1,36 +1,64 @@
 from flask import Flask, url_for
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def mission():
     return 'Миссия Колонизация Марса'
 
 
 @app.route('/index')
-def motto():
-    return 'И на Марсе будут яблони цвести!'
+def index():
+    return "И на Марсе будут яблони цвести!"
 
 
 @app.route('/promotion')
-def promo():
-    return '<br>'.join(['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
-                        'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!'])
+def promotion():
+    lines = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+             'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
+    return '</br>'.join(lines)
 
 
-@app.route('/image')
-def image():
-    return f'''<!doctype html>
-                    <html lang="en"
-                     <head>
-                      <meta charset="utf-8">
-                       <title>Привет, Марс</title>
-                     </head>
-                     <body>
-                       <h1>Миссия Колонизация Марса</h1>
-                       <img src={url_for('static', filename='images.jpg')}
-                        alt="Картинка не найдена">
-                        <p>Вот она какая, красная планета!</p>
-                     </body>
-                    </html>'''
+@app.route('/image_mars')
+def image_mars():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src={url_for('static', filename='img/mars.png')}>
+                    <p>Вот она какая, красная планета!</p>
+                  </body>
+                </html>"""
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" href="{url_for('static', filename='css/style.css')}">
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src={url_for('static', filename='img/images.jpg')}>
+                    <p class="alert alert-primary" role="alert">Человечество вырастает из детства.</p>
+                    <p class="alert alert-success" role="alert">Человечеству мала одна планета.</p>
+                    <p class="alert alert-secondary" role="alert">Мы сделаем обитаемыми безжизненные пока планеты.</p>
+                    <p class="alert alert-danger" role="alert">И начнем с Марса!</p>
+                    <p class="alert alert-warning" role="alert">Присоединяйся!</p>
+                  </body>
+                </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
